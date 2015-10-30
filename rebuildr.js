898cbcbd21486aa1939964964619579cbc78a450
Giancarlo Salamanca <giancarlo.salamanca@envato.com>
2015-10-30 15:20:41 +1100

    Rebuildr Enterprise

--- rebuildr.js
+++ rebuildr.js
@@ -1,15 +1,38 @@
-function watchForFailedBuild() {
-  if (document.querySelector('.build-state-started .build-pipeline-state-failed.build-pipeline-job-script')) {
+function clickRebuildButton() {
+  rebuildButton = document.querySelector('.build-rebuild-button');
+  if (rebuildButton) {
     clearInterval(window.rebuildrInterval);
-    console.log('Build failure detected in running build!');
-    document.querySelector('.build-cancel-button').click();
-    setTimeout(function() {
-      document.querySelector('.build-rebuild-button').click();
-    }, 2000);
+    console.log('[Rebuildr] Triggering rebuild');
+    rebuildButton.click();
+  }
+}
+
+function watchForFailedBuild() {
+  if (document.querySelector('.build-pipeline-state-failed.build-pipeline-job-script')) {
+    console.log('[Rebuildr] Build failed!');
+    clearInterval(window.rebuildrInterval);
+    cancelButton = document.querySelector('.build-cancel-button');
+    window.rebuildrInterval = setInterval(clickRebuildButton, 1000);
+    if (cancelButton) {
+      console.log('[Rebuildr] Cancelling build');
+      cancelButton.click();
+    } else {
+      clickRebuildButton();
+    }
+  }
+}
+
+function watchForRunningBuild() {
+  if (document.querySelector('.build-state-started')) {
+    console.log('[Rebuildr] Build running...');
+    clearInterval(window.rebuildrInterval);
+    window.rebuildrInterval = setInterval(watchForFailedBuild, 1000);
+    watchForFailedBuild();
   }
 }
 
 if (document.querySelector('#build')) {
-  console.log('Rebuildr extension loaded - build detected');
-  window.rebuildrInterval = setInterval(watchForFailedBuild, 1000);
+  console.log('[Rebuildr] Extension loaded - Buildkite build detected');
+  window.rebuildrInterval = setInterval(watchForRunningBuild, 1000);
+  watchForRunningBuild();
 }
