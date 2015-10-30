11a52a013cb406799987accb0b421d82889877c2
Giancarlo Salamanca <giancarlo.salamanca@envato.com>
2015-10-30 14:00:01 +1100

    Only rebuild in-progress builds

--- rebuildr.js
+++ rebuildr.js
@@ -1,7 +1,7 @@
 console.log('Rebuildr extension loaded!');
 
 function watchForFailedBuild() {
-  if (document.querySelectorAll('#build .build-pipeline-state-failed.build-pipeline-job-script').length > 0) {
+  if (document.querySelectorAll('#build .build-state-started .build-pipeline-state-failed.build-pipeline-job-script').length > 0) {
     console.log('Build failure detected!');
     buttonToMash = document.querySelector('.build-cancel-button, .build-rebuild-button');
     if (buttonToMash) {
